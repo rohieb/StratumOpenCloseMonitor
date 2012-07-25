23b558d57ce470d2f7e08ef6ffd7a6ca232d5828
Roland Hieber <rohieb@rohieb.name>
2012-07-25 21:25:36 +0200

    MediaWiki plugin: URL has changed, slightly improve README

--- mediawiki/StratumOpenCloseSidebar.php
+++ mediawiki/StratumOpenCloseSidebar.php
@@ -3,8 +3,8 @@ $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'stratumOpenCloseSidebarHook';
 function stratumOpenCloseSidebarHook($skin, $tpl) {
   if(isset($tpl->data['sidebar']['Space-Status']))
     $tpl->data['sidebar']['Space-Status'] = '<ul><li><a title="'.
-       '&Ouml;ffnungsstatus" href="/mediawiki/index.php/Open/Close-Monitor">'.
-       '<img style="border:none; height:2em;" src="//rohieb.name/stratum0/'.
+       '&Ouml;ffnungsstatus" href="/wiki/Open/Close-Monitor">'.
+       '<img style="border:none; height:2em;" src="http://status.stratum0.org/'.
        'status.png" /></a></li></ul>';
   return true;
 }
